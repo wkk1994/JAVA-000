541bd2aa5f411b3734af9888e5739db1601c55a3
wangkunkun <wkk1994op@163.com>
2020-11-12 00:33:51 +0800

    提交Week4练习和作业

---
+++ thread-code/src/main/java/com/wkk/lean/java/thread/homework/AbstractSum.java
@@ -0,0 +1,21 @@
+package com.wkk.lean.java.thread.homework;
+
+/**
+ * @Description 计算斐波那契数
+ * @Author Wangkunkun
+ * @Date 2020/11/11 21:55
+ */
+public abstract class AbstractSum {
+
+    abstract void printSum(int num);
+
+    protected static int sum(int num) {
+        return fibo(num);
+    }
+
+    private static int fibo(int a) {
+        if ( a < 2)
+            return 1;
+        return fibo(a-1) + fibo(a-2);
+    }
+}
---
+++ thread-code/src/main/java/com/wkk/lean/java/thread/homework/ModeOne.java
@@ -0,0 +1,33 @@
+package com.wkk.lean.java.thread.homework;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Description 方式一：通过Callable + FutureTask + Thread实现线程调用并获取处理结果
+ * @Author Wangkunkun
+ * @Date 2020/11/11 21:57
+ */
+public class ModeOne extends AbstractSum {
+
+    public void printSum(final int num) {
+        // 在这里创建一个线程或线程池
+        long start = System.currentTimeMillis();
+        Callable<Integer> task = new Callable<Integer>() {
+            public Integer call() throws Exception {
+                return sum(num);
+            }
+        };
+        FutureTask<Integer> futureTask = new FutureTask(task);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        try {
+            Integer result = futureTask.get();
+            System.out.println("异步计算结果为：" + result);
+            System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 然后退出main线程
+    }
+}
